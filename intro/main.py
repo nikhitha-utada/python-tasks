3f0a2683e98ed871213b5ced4bb9ab3bb1280e46
nikhitha-utada <nikhithautada2003@gmail.com>
2025-01-24 12:59:24 +0530

    intro

---
+++ intro/main.py
@@ -0,0 +1,55 @@
+# to distinguish var names with strings or characters we use single or double quotes
+
+# max_len = index+1
+# max_index = len-1
+
+
+print(-3 + -5)
+print(5 * 6)
+print(-3 * -8)
+print(10/2)
+print(5-6)
+print(5+6-8/2)
+print(2/54)
+
+a=1234567
+b=3456678
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+
+num1 = 2
+num2 = 5
+print(num1+num2) # 7
+print(num-num2)  # -3
+print(num1*num2) # 10
+print(num1/num2) # 2.5
+
+num2 = 10
+print(num1+num2) #12
+
+num1 = 5
+print(num1*num2)  #20
+
+num1 = -2 
+print(num1/num2) # 0.2..
+
+num1 = 20
+print(0**0)
+
+good_morning = 123
+print(good_morning)
+greet = "good morning"
+print(greet)
+
+str = "abcdef ghi"
+print(len(str))
+print(str[0])
+print(str[len(str)-1])
+
+
+
+
+
+
