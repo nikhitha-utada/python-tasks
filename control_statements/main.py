73dcd36723e00caccd160b263b3694ce3f09c7c3
nikhitha-utada <nikhithautada2003@gmail.com>
2025-01-30 09:40:30 +0530

    if else statement

--- control_statements/main.py
+++ control_statements/main.py
@@ -10,8 +10,11 @@
     # --> preferred indentation is 4 spaces(1 tab)
     # --> there is no rule that both if and else should have same number of spaces but the only thing that is considered is single block of statements should have same level of indentation
 
+
+# There is an issue here that is if we provide 0 as an input, then it prints negative 
 num = int(input("Enter the number: "))
 if num>0:
     print(f"{num} is Positive")
 else:
     print(f"{num} is Negative")
+
