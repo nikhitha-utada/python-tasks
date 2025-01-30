ced2c64420dbbd471767a1477fa2e63b781b83ca
nikhitha-utada <nikhithautada2003@gmail.com>
2025-01-30 09:32:49 +0530

    if else statement

---
+++ control_statements/main.py
@@ -0,0 +1,17 @@
+# Conditional statements:
+#if statement:
+# if condition:
+    # statements
+# else:
+    #statements
+# Note:
+    # Indentation specifies that the code belongs to a same block of code
+    # --> atleast one space is required to define the same level block
+    # --> preferred indentation is 4 spaces(1 tab)
+    # --> there is no rule that both if and else should have same number of spaces but the only thing that is considered is single block of statements should have same level of indentation
+
+num = int(input("Enter the number: "))
+if num>0:
+    print(f"{num} is Positive")
+else:
+    print(f"{num} is Negative")
