044e55a370baf28b5e5747a7ad6213e9d25becfc
nikhitha-utada <nikhithautada2003@gmail.com>
2025-01-31 09:20:36 +0530

    for loop

---
+++ control_statements/loops.py
@@ -0,0 +1,38 @@
+#LOOPS:
+# To do repeated tasks
+# Uses:
+#  --> Time
+#  --> Money
+#  --> Efficiency
+#  --> Readable
+#  --> less lines of code
+#  --> Easier to maintain
+#  Types of loops:
+    # for loop
+    # while loop
+    # do while
+
+#  for loop:
+# syntax : for i in range(start,end):
+#               statements
+
+
+# To print even numbers in the range
+# Method-01:
+for i in range(0,21):
+    if i%2==0:
+        print(i)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
