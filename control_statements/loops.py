801baf7c41dafdd9ed122cfd30421efe943257f7
nikhitha-utada <nikhithautada2003@gmail.com>
2025-01-31 10:49:55 +0530

    while loop

--- control_statements/loops.py
+++ control_statements/loops.py
@@ -46,10 +46,56 @@ for i in range(1,10,2):
 for i in range(1,50):
     print(i**2)
 
+# Nested loops:
+# syntax: for i in range(start,end):
+#           for j in range(start,end):
+#                 statements
 
+for outer in range(1,11):
+    for inner in range(1,11):
+        print(outer,inner) 
 
+# while loops:
+# the loop runs until the condition fails
+# ctrl c --> keyboard interruption
+# syntax : while condition:
+#               statements
 
+num=10
+while(num>5):
+    print(num)
+    num-=1
 
+# When to prefer for and while loops:
+#  we prefer for loop when we know the number of iterations and while loop when we dont know the number of iterations
+
+num = 5
+while(num<26):
+    if(i%2==0):
+        print(i,"even")
+    else:
+        print(i,"odd")
+    num+=1
+
+# Problem-02:
+num = 5
+while(num<26):
+    if(num%2==0):
+        print(num,"even")
+    else:
+        print(num,"odd")
+    num+=1
+
+# converting for to while:
+outer = 1
+inner = 1
+while outer < 11:
+    if outer!=5 and outer!=7:
+        # inner = 1
+        while inner < 11:
+            print(outer,inner)
+            inner+=1
+    outer+=1
 
 
 
