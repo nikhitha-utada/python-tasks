81157b73d661e8332ed34a77d09828d91a8970cc
nikhitha-utada <nikhithautada2003@gmail.com>
2025-02-05 23:29:43 +0530

    reverse a number method-2

--- 05-02-25/3.py
+++ 05-02-25/3.py
@@ -9,3 +9,19 @@ while num>0:
     rev = rev * 10 + last
     num//=10
 print(rev)
+
+#Method-2:
+
+num=1234
+temp=num
+len=0
+while temp>0:
+    temp=temp//10
+    len+=1
+res=0
+# count=len
+while num>0:
+    res+=(num%10)(10*(len-1))
+    num=num//10
+    len-=1
+print(res)
